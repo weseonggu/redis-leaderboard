c6b4388ad22382f5aff3e8b93965416ae114b35d
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-08-07 13:46:33 +0900

    상품 구매시 redis에 1증가 하기

--- src/main/java/com/example/redis/ItemService.java
+++ src/main/java/com/example/redis/ItemService.java
@@ -1,10 +1,13 @@
 package com.example.redis;
 
 import com.example.redis.domain.Item;
+import com.example.redis.domain.ItemDto;
 import com.example.redis.domain.ItemOrder;
 import com.example.redis.repository.ItemRepository;
 import com.example.redis.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,13 +17,16 @@ import org.springframework.web.server.ResponseStatusException;
 public class ItemService {
     private final ItemRepository itemRepository;
     private final OrderRepository orderRepository;
+    private final ZSetOperations<String, ItemDto> rankOps;
 
     public ItemService(
             ItemRepository itemRepository,
-            OrderRepository orderRepository
+            OrderRepository orderRepository,
+            RedisTemplate<String, ItemDto> redisTamplate
     ) {
         this.itemRepository = itemRepository;
         this.orderRepository = orderRepository;
+        this.rankOps = redisTamplate.opsForZSet();//opsForZSet() sorted set 타입의 명령어를 쓰기 위생서 사용
     }
 
     public void purchase(Long id) {
@@ -30,6 +36,8 @@ public class ItemService {
                 .item(item)
                 .count(1)
                 .build());
+
+        rankOps.incrementScore("soldRanks", ItemDto.fromEntity(item), 1);
     }
 
 }
